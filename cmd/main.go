40394367e7393d132c04c80ba20ce91367a48fc5
0xlax <harshu8310@gmail.com>
2023-05-19 14:51:37 +0530

    Calling ConnectDB from main.go

--- cmd/main.go
+++ cmd/main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"log"
 
+	"github.com/0xlax/accuknox-rest/database"
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
+	database.ConnectDb()
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
